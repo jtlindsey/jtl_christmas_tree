91941645ab153f7b0fa94555d316fcb7740f1f1a
Nestor Castillo <nestor.castillo@izea.com>
2014-12-04 08:17:04 -0500

    Progress from Dojo. Merry Christmas

--- spec/christmas_tree_spec.rb
+++ spec/christmas_tree_spec.rb
@@ -2,4 +2,77 @@ require './lib/christmas_tree'
 
 describe ChristmasTree do
 
+  let(:tree){ChristmasTree.new(3)}
+
+  context "#initialize" do
+    it "contains a height" do
+
+      expect(tree.height).to eq 3
+    end
+
+    it "contains a width double the height" do
+
+      expect(tree.width).to eq 6
+    end
+
+    it "raises an error if height is less than 3" do
+      expect { ChristmasTree.new(2) }.to raise_error
+    end
+  end
+
+  context "#drawing_stars" do
+    it "draws two spaces and a star" do
+      blank_spaces = 2
+      stars = 1
+
+      expect(tree.drawing_stars(blank_spaces, stars)).to eq "  *\n"
+    end
+
+    it "draws one space and two stars" do
+      blank_spaces = 1
+      stars = 2
+
+      expect(tree.drawing_stars(blank_spaces, stars)).to eq " **\n"
+    end
+  end
+
+  context "#draw_tree" do
+    it "draws a tree with a height of 3" do
+      expected = <<-ct
+  *
+ ***
+*****
+      ct
+
+      expect(tree.draw_tree).to eq expected
+    end
+  end
+
+  context "#draw_base" do
+    it "draws a tree with a base" do
+      expected = <<-ct
+  x
+  x
+  x
+      ct
+
+      expect(tree.draw_base).to eq expected
+    end
+  end
+
+  context "#draw_tree_with_base" do
+    it "draws a Christmas Tree" do
+      expected = <<-ct
+  *
+ ***
+*****
+  x
+  x
+  x
+      ct
+
+      expect(tree.draw_tree_with_base).to eq expected
+
+    end
+  end
 end
